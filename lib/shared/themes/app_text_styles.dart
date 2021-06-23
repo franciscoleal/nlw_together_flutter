12417cf367d60a1ab45b4bd382e7ba24992603aa
Francisco Leal <francisco.lleal@gmail.com>
2021-06-22 21:40:36 -0300

    atualização do path appcolors para app_colors.dart

--- lib/shared/themes/app_text_styles.dart
+++ lib/shared/themes/app_text_styles.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:pay_flow/shared/themes/appcolors.dart';
+import 'package:pay_flow/shared/themes/app_colors.dart';
 
 class TextStyles {
   static final titleHome = GoogleFonts.lexendDeca(
